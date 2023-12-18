6d6f1fc64384e2c230510b74f33d06ee022a2fd7
Victor Hugo Queiroga <victor.vqs@hotmail.com>
2023-12-18 12:07:48 -0300

    Update and finish tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,16 @@
 /// <reference types="cypress" />
 
-import dayjs from 'dayjs';
+import { format, prepareLocalStorage } from '../support/utils'
 
 context('Dev Finances', () => {
   beforeEach(() => {
-    cy.visit('https://victorhq.github.io/maratona-discover---RocketSeat/')
+    cy.visit('https://victorhq.github.io/maratona-discover---RocketSeat/', {
+      onBeforeLoad: (win) => {
+        prepareLocalStorage(win)
+      }
+    })
   })
-
+ 
   it('Must record entries', () => {
     cy.get('#transaction > .button').click()
     cy.get('#description').type('Salário')
@@ -14,7 +18,7 @@ context('Dev Finances', () => {
     cy.get('#date').type('2023-11-07');
     cy.get('button').contains('Salvar').click()
 
-    cy.get('#data-table tbody tr').should('have.length', 1)
+    cy.get('#data-table tbody tr').should('have.length', 3)
   })
 
   it('Must register exits', () => {
@@ -24,24 +28,24 @@ context('Dev Finances', () => {
     cy.get('#date').type('2023-12-31');
     cy.get('button').contains('Salvar').click()
 
-    cy.get('#data-table tbody tr').should('have.length', 1)
+    cy.get('#data-table tbody tr').should('have.length', 3)
   })
 
-  it.only('Remove entries and exits', () => {
-    const entrie = 'Salário';
-    const exit = 'Imposto';
+  it('Remove entries and exits', () => {
+    const entrie = 'Mesada';
+    const exit = 'Suco';
 
-    cy.get('#transaction > .button').click()
-    cy.get('#description').type('Salário')
-    cy.get('[name=amount]').type(1850)
-    cy.get('#date').type('2023-11-07');
-    cy.get('button').contains('Salvar').click()
+    // cy.get('#transaction > .button').click()
+    // cy.get('#description').type('Salário')
+    // cy.get('[name=amount]').type(1850)
+    // cy.get('#date').type('2023-11-07');
+    // cy.get('button').contains('Salvar').click()
 
-    cy.get('#transaction > .button').click()
-    cy.get('#description').type('Imposto Sindical')
-    cy.get('[name=amount]').type(-1850 * 1 / 100)
-    cy.get('#date').type('2023-12-31');
-    cy.get('button').contains('Salvar').click()
+    // cy.get('#transaction > .button').click()
+    // cy.get('#description').type('Imposto Sindical')
+    // cy.get('[name=amount]').type(-1850 * 1 / 100)
+    // cy.get('#date').type('2023-12-31');
+    // cy.get('button').contains('Salvar').click()
 
     cy.get('td.description')
       .contains(entrie)
@@ -56,5 +60,47 @@ context('Dev Finances', () => {
       .click()
 
     cy.get('#data-table tbody tr').should('have.length', 0)
+  })
+
+  it('Should validate transactions', () => {
+    // const entrie = 'Salário';
+    // const exit = 'Imposto';
+
+    // cy.get('#transaction > .button').click()
+    // cy.get('#description').type('Salário')
+    // cy.get('[name=amount]').type(1850)
+    // cy.get('#date').type('2023-11-07');
+    // cy.get('button').contains('Salvar').click()
+
+    // cy.get('#transaction > .button').click()
+    // cy.get('#description').type('Imposto Sindical')
+    // cy.get('[name=amount]').type(-500 * 1 / 100)
+    // cy.get('#date').type('2023-12-31');
+    // cy.get('button').contains('Salvar').click()
+
+    let incomes = 0
+    let expanses = 0
+
+    cy.get('#data-table tbody tr')
+      .each(($el, index, $list) => {
+        cy.get($el).find('td.income, td.expense').invoke('text').then(text => {
+            if(text.includes('-')){
+              expanses = expanses + format(text)
+            } else {
+              incomes = incomes + format(text)
+            }
+
+            cy.log(`Entradas: ${incomes}`)
+            cy.log(`Saídas: ${expanses}`)
+        })
+      })
+
+    cy.get('#totalDisplay').invoke('text').then(text => {
+
+      let formattedTotalDisplay = format(text)
+      let expectedTotal = incomes + expanses 
+
+      expect(formattedTotalDisplay).to.eq(expectedTotal)
+    })
   })
 })
---
+++ cypress/support/utils.js
@@ -0,0 +1,30 @@
+export const format = (value) => {
+    let formattedValue
+
+    formattedValue = value.replace(/,/g, '.').replace(/\.(?=.*\.)/g, '');
+    formattedValue = Number(formattedValue.split('$')[1].trim())
+
+    formattedValue = value.includes('-') ? -formattedValue : formattedValue
+
+    return formattedValue
+}
+
+export const randomNumber = () => {
+    return Math.floor(Math.random() * 101)
+}
+
+export const prepareLocalStorage = (win) => {
+    win.localStorage.setItem('dev.finances:transactions', JSON.stringify([
+        {
+            description: "Mesada",
+            amount: randomNumber() * 100,
+            date: "11/03/2021"
+        },
+        {
+            description: "Suco",
+            amount: - (randomNumber() * 100),
+            date: "12/03/2021"
+        }
+    ])
+    )
+}
